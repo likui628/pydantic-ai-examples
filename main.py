f799a17ab9e9b7357afd22f0198002af20396f45
likui628 <likui.it@gmail.com>
2026-01-07 20:16:53 +0800

    hello world

---
+++ main.py
@@ -0,0 +1,21 @@
+from pydantic_ai.providers.openrouter import OpenRouterProvider
+from pydantic_ai.models.openrouter import OpenRouterModel
+from pydantic_ai import Agent
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+model = OpenRouterModel(
+    'anthropic/claude-haiku-4.5',
+    provider=OpenRouterProvider(api_key=os.getenv('API_KEY')),
+)
+
+agent = Agent(
+    model,
+    instructions='Be concise, reply with one sentence.',
+)
+
+result = agent.run_sync('Where does "hello world" come from?')
+print(result.output)
